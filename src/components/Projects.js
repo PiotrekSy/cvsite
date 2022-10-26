46c671ca97b9c933ea49f4154281a1b02c2b66e9
PiotrekSy <piotrsyjud@gmail.com>
2022-10-26 14:40:48 +0200

    icons

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -10,24 +10,59 @@ const Projects = () => {
             <div className="container">
                 <div className="firstRow">
                     <div className="item">
-                        <div className="foodIcon"></div>
+                        <div className="resumeIcon"></div>
+                        <div className="projectName">ResumeSite</div>
                         <div className="link">
-                            <a href="https://zaplanujjedzonko-ps.netlify.app/">This Site</a>
-                            <a href="https://zaplanujjedzonko-ps.netlify.app/">ZaplanujJedzonko App</a>
+                            <a href="/"
+                                target="_blank" rel="noreferrer">This Site</a>
+                            <a href="https://github.com/PiotrekSy/cvsite"
+                                target="_blank" rel="noreferrer">GitHub</a>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <div className="clothesIcon"></div>
+                        <div className="projectName">GiveAwayApp </div>
+                        <div className="link">
+                            <a href="https://giveawaysite.netlify.app/"
+                                target="_blank" rel="noreferrer">App</a>
+                            <a href="https://github.com/PiotrekSy/GiveAway"
+                                target="_blank" rel="noreferrer">GitHub</a>
                         </div>
                     </div>
                     <div className="item">
                         <div className="foodIcon"></div>
+                        <div className="projectName">ZaplanujJedzonko</div>
                         <div className="link">
-                            <a href="https://zaplanujjedzonko-ps.netlify.app/">App</a>
-                            <a href="https://zaplanujjedzonko-ps.netlify.app/">ZaplanujJedzonko App</a>                    </div>
+                            <a href="https://zaplanujjedzonko-ps.netlify.app/"
+                                target="_blank" rel="noreferrer">App</a>
+                            <a href="https://github.com/PiotrekSy/zaplanuj_jedzonko"
+                                target="_blank" rel="noreferrer">GitHub</a>
+                        </div>
+                    </div>
+                </div>
+                <div className="secondRow">
+                    <div className="item">
+                        <div className="questionIcon"></div>
+                        <div className="projectName">Comming soon </div>
+                        <div className="link">
+                            <a href="/" target="_blank" rel="noreferrer">App</a>
+                            <a href="blank" target="_blank" rel="noreferrer">GitHub</a>
+                        </div>
                     </div>
                     <div className="item">
-                        <div className="clothesIcon"></div>
-                        <div className=""></div>
+                        <div className="questionIcon"></div>
+                        <div className="projectName">Comming soon </div>
                         <div className="link">
-                            <a href="https://giveawaysite.netlify.app/" >GiveAway App</a>
-                            <a href="https://giveawaysite.netlify.app/" >GiveAway App</a>
+                            <a href="/" target="_blank" rel="noreferrer">App</a>
+                            <a href="blank" target="_blank" rel="noreferrer">GitHub</a>
+                        </div>
+                    </div>
+                    <div className="item">
+                        <div className="questionIcon"></div>
+                        <div className="projectName">Comming soon</div>
+                        <div className="link">
+                            <a href="/" target="_blank" rel="noreferrer">App</a>
+                            <a href="blank" target="_blank" rel="noreferrer">GitHub</a>
                         </div>
                     </div>
                 </div>
